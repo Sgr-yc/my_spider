b4c43e6816ccd84bf363f93587ca86517fe50472
yangcai <Yang00>
2020-07-21 13:47:26 +0800

    第三方库

---
+++ upgrade_lib.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+# @Project_Name :   LibraTest
+# @File         :   update_start.py
+# @Author       :   'yangcai'
+# @Time         :   2020/4/3 16:10
+import os
+
+
+def install_lib(library_list):
+    """
+    安装库
+    :param library_list:
+    :return:
+    """
+    for lib in library_list:
+        os.system("pip install -i https://pypi.tuna.tsinghua.edu.cn/simple " + lib)
+
+
+def upgrade_lib(upgrade_list):
+    """
+    更新库
+    :param upgrade_list:
+    :return:
+    """
+    for lib in upgrade_list:
+        os.system("pip install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple " + lib)
+
+
+if __name__ == '__main__':
+    library_install_list = ['scrapy', 'wheel', 'Twisted']
+    library_upgrade_list = []
+    install_lib(library_install_list)
+    upgrade_lib(library_upgrade_list)
