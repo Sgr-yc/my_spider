1592b71d7282c7675ff6dda784a7a17d9e8ec795
yangcai <Yang00>
2020-07-31 15:34:19 +0800

    爬虫

--- requests_article/movie_spider.py
+++ requests_article/movie_spider.py
@@ -4,68 +4,67 @@
 # @Author       :   'yangcai'
 # @Time         :   2020/7/30 17:42
 import os
-import re
-import time
-
+import ffmpy3
 import requests
 from bs4 import BeautifulSoup
+from multiprocessing.dummy import Pool as ThreadPool
 
-from lxml import etree
+search_keyword = '越狱第一季'
+search_url = 'http://www.jisudhw.com/index.php'
+serach_params = {
+    'm': 'vod-search'
+}
+serach_headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36',
+    'Referer': 'http://www.jisudhw.com/',
+    'Origin': 'http://www.jisudhw.com',
+    'Host': 'www.jisudhw.com'
+}
+serach_datas = {
+    'wd': search_keyword,
+    'submit': 'search'
+}
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 OPR/66.0.3515.115'}
+video_dir = ''
 
+r = requests.post(url=search_url, params=serach_params, headers=serach_headers, data=serach_datas)
+r.encoding = 'utf-8'
+server = 'http://www.jisudhw.com'
+search_html = BeautifulSoup(r.text, 'lxml')
+search_spans = search_html.find_all('span', class_='xing_vb4')
+for span in search_spans:
+    url = server + span.a.get('href')
+    name = span.a.string
+    print(name)
+    print(url)
+    video_dir = name
+    if name not in os.listdir('./'):
+        os.mkdir(name)
 
-def cartoon(folder_name):
-    full_name = os.getcwd()
-    folder_name = os.path.join(full_name, '漫画', folder_name)
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-    url = 'https://www.dmzj.com/info/benghuaitongrenmanhua.html'
-    response = requests.get(url, headers=headers)
-    html = etree.HTML(response.text)
-    url_list = html.xpath('//div[@class="tab-content zj_list_con autoHeight"]/ul/li/a/@href')
-    name_list = html.xpath('//div[@class="tab-content zj_list_con autoHeight"]/ul/li/a/@title')
-    car_list = []
-    for num in range(len(url_list)):
-        cartoon_dict = dict()
-        cartoon_dict['name'] = name_list[num].replace(' ', '')
-        cartoon_dict['url'] = url_list[num].replace(' ', '')
-        car_list.append(cartoon_dict)
-    download_cartoon(car_list, folder_name)
-
-
-def download_cartoon(car_list, full_name):
-    for car in car_list:
-        folder_name = os.path.join(full_name, car['name'])
-        if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
-        download_header = {
-            'Referer': car['url']
-        }
-        car_response = requests.get(car['url'], headers=headers)
-        html = BeautifulSoup(car_response.text, 'lxml')
-        script_info = html.script
-        pics = re.findall(r'\d{13,14}', str(script_info))
-        for j, pic in enumerate(pics):
-            if len(pic) == 13:
-                pics[j] = pic + '0'
-        pics = sorted(pics, key=lambda x: int(x))
-        car_hou = re.findall(r'\|(\d{6})\|', str(script_info))[0]
-        car_qia = re.findall(r'\|(\d{5})\|', str(script_info))[0]
-        for idx, pic in enumerate(pics):
-            if pic[-1] == '0':
-                car_url = 'https://images.dmzj.com/img/chapterpic/' + car_qia + '/' + car_hou + '/' + pic[:-1] + '.jpg'
-            else:
-                car_url = 'https://images.dmzj.com/img/chapterpic/' + car_qia + '/' + car_hou + '/' + pic + '.jpg'
-            car_response = requests.get(car_url, headers=download_header)
-            pic_name = '%03d.jpg' % (idx + 1)
-            pic_save_path = os.path.join(folder_name, pic_name)
-            with open(pic_save_path, "wb") as file:
-                file.write(car_response.content)
-        time.sleep(1)
+    detail_url = url
+    r = requests.get(url=detail_url)
+    r.encoding = 'utf-8'
+    detail_bf = BeautifulSoup(r.text, 'lxml')
+    num = 1
+    serach_res = {}
+    for each_url in detail_bf.find_all('input'):
+        if 'm3u8' in each_url.get('value'):
+            url = each_url.get('value')
+            if url not in serach_res.keys():
+                serach_res[url] = num
+            print('第%03d集:' % num)
+            print(url)
+            num += 1
 
 
-if __name__ == '__main__':
-    cartoon_name = '崩坏同人漫画'
-    cartoon(cartoon_name)
+def down_video(url):
+    num = serach_res[url]
+    video_name = os.path.join(video_dir, '第%03d集.mp4' % num)
+    ffmpy3.FFmpeg(inputs={url: None}, outputs={video_name: None}).run()
+
+
+# 开8个线程池
+pool = ThreadPool(8)
+results = pool.map(down_video, serach_res.keys())
+pool.close()
+pool.join()
